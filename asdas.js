da4bed755c9e2fb45be08f0763c659fc30f70c68
insubordinate <axl.cruz8344@gmail.com>
2023-01-31 12:52:36 -0500

    added more stuff

---
+++ asdas.js
@@ -0,0 +1,23 @@
+
+function wow(){
+
+    let x = 1 
+    if(x===1){
+        var y = 'wow'
+    }
+    return y
+}
+
+
+function wow2(){
+
+    let x = 1 
+    if(x===1){
+        let z = 'wow'
+    }
+    return z
+}
+
+
+console.log(wow())
+console.log(wow2())
